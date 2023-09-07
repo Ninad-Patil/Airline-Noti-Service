98ca8e6e4e16f4b0c0b35870f444fdf890894968
ninad patil <ninad.patil198@gmail.com>
2023-09-07 12:15:18 +0530

    added create ticket api

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const { serverConfig, logger } = require("./config");
 const apiRoutes = require("./routes");
 const express = require("express");
 
-const mailsender = require("./config/email-config");
 const app = express();
 
 app.use(express.json());
@@ -13,15 +12,4 @@ app.use("/api", apiRoutes);
 app.listen(serverConfig.PORT, () => {
   console.log(`server running on port ${serverConfig.PORT}`);
   logger.info("Successfully started the server", "root", {});
-  try {
-    const response = mailsender.sendMail({
-      from: serverConfig.GMAIL_EMAIL,
-      to: "ninad.patil198@gmail.com",
-      subject: "testing if the service is working or not",
-      text: "hoping it works",
-    });
-    console.log(response);
-  } catch (error) {
-    console.log(error);
-  }
 });
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { infoController } = require("../../controllers");
+const { infoController, emailController } = require("../../controllers");
 
 const router = express.Router();
 
 router.get("/info", infoController.info);
+router.post("/tickets", emailController.create);
 
 module.exports = router;
